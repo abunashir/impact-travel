e2e25c55b053a1a1a9a7350b05959bdde26c440a
Abu Nashir <abunashir@gmail.com>
2016-10-26 12:24:22 +0700

    Fix missing translation for hotels
    In the `hotel` provider we were manually specifying the page
    attribute, instead of using the configuration option. This
    was causing issue with configuring content. This commit fix
    it by adding configuration support for `hotel` providers.

--- app/controllers/impact_travel/hotels_controller.rb
+++ app/controllers/impact_travel/hotels_controller.rb
@@ -5,10 +5,5 @@ module ImpactTravel
     def provider_type
       @provider_type ||= "hotel"
     end
-
-    def set_page_attributes
-      @page_title = "Hotels"
-      @page_subtitle = "This is the sub title for the hotel"
-    end
   end
 end
