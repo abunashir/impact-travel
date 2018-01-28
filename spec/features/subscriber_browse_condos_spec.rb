80ce16433b65bbd66c0a108a6873a2f9010c3c34
Abu Nashir <abunashir@gmail.com>
2018-01-28 13:39:58 +0100

    Add resources tab with public links
    Recently, we've decided to add the public link in one place, so it
    seems more reasonable to put those in one place, so people can find
    those in one place and also easier for us to manage in the future.

--- spec/features/subscriber_browse_condos_spec.rb
+++ spec/features/subscriber_browse_condos_spec.rb
@@ -7,7 +7,9 @@ feature "Condos Provider" do
     stub_provider_listing_api("resort")
     stub_provider_find_by_slug_api(provider_slug)
 
-    click_on "Condos"
+    click_on "RESOURCES"
+    click_on "Condos", match: :first
+
     click_on "Browse now", match: :first
 
     expect(current_path).to eq(impact_travel.condo_path(provider_slug))
